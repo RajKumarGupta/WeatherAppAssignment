0ee7623f4968a4d70769409e47b5346580e5dc07
Raj Kumar Gupta <rajkgupta.me@gmail.com>
2018-06-03 14:37:13 +0530

    Created FakeLocalAPI Class

---
+++ app/src/test/java/com/assignment/app/data/entity/mapper/data/FakeLocalAPI.java
@@ -0,0 +1,51 @@
+package com.assignment.app.data.entity.mapper.data;
+
+import com.assignment.app.data.entity.CountryEntity;
+
+public class FakeLocalAPI {
+
+  private static final String JSON_RESPONSE_COUNTRY = "{\n"
+          + "    \"country_id\": \"3\",\n"
+          + "    \"flag_image_url\": \"https://d3xm8g2fmv6ji.cloudfront.net/img/flags/australia@2x.png\",\n"
+          + "    \"country_name\": \"Australia\"\n"
+          + "  }";
+
+  private static final String JSON_RESPONSE_COUNTRY_COLLECTION = "[\n"
+          + "  {\n"
+          + "    \"country_id\": \"3\",\n"
+          + "    \"flag_image_url\": \"https://d3xm8g2fmv6ji.cloudfront.net/img/flags/australia@2x.png\",\n"
+          + "    \"country_name\": \"Australia\"\n"
+          + "  },\n"
+          + "  {\n"
+          + "    \"country_id\": \"12\",\n"
+          + "    \"flag_image_url\": \"https://d3xm8g2fmv6ji.cloudfront.net/img/flags/australia@2x.png\",\n"
+          + "    \"country_name\": \"Cambodia\"\n"
+          + "  },\n"
+          + "  {\n"
+          + "    \"country_id\": \"4\",\n"
+          + "    \"flag_image_url\": \"https://d3xm8g2fmv6ji.cloudfront.net/img/flags/australia@2x.png\",\n"
+          + "    \"country_name\": \"Hong Kong\"\n"
+          + "\n"
+          + "  }]";
+
+  public static String getJsonResponseCountry() {
+    return JSON_RESPONSE_COUNTRY;
+  }
+
+  public static String getJsonResponseCountryCollection() {
+    return JSON_RESPONSE_COUNTRY_COLLECTION;
+  }
+
+  private final static String FAKE_COUNTRY_NAME = "Australia";
+  private final static String FAKE_COUNTRY_IMG = "https://d3xm8g2fmv6ji.cloudfront.net/img/flags/australia@2x.png";
+
+  public static CountryEntity getFakeCountryEntity() {
+
+
+    CountryEntity countryEntity = new CountryEntity();
+    countryEntity.setCountry_name(FAKE_COUNTRY_NAME);
+    countryEntity.setFlag_image_url(FAKE_COUNTRY_IMG);
+
+    return countryEntity;
+  }
+}
